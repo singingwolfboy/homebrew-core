5eb4677684249285cd8f0c38598fdfb0fe3e2dd8
David Baumgold <david@davidbaumgold.com>
2022-11-21 14:34:04 -0500

    moto: migrate to python@3.11

--- Formula/moto.rb
+++ Formula/moto.rb
@@ -19,7 +19,7 @@ class Moto < Formula
 
   depends_on "rust" => :build # for cryptography
   depends_on "jsonschema"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
   depends_on "pyyaml"
   depends_on "six"
 
@@ -276,7 +276,7 @@ class Moto < Formula
   def install
     virtualenv_install_with_resources
     # we depend on jsonschema, but that's a separate formula, so install a `.pth` file to link them
-    site_packages = Language::Python.site_packages("python3.10")
+    site_packages = Language::Python.site_packages("python3.11")
     jsonschema = Formula["jsonschema"].opt_libexec
     (libexec/site_packages/"homebrew-jsonschema.pth").write jsonschema/site_packages
   end
